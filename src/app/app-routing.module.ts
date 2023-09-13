9d0707205da0f5fccebab7d5b4c9f307ca1fdcb5
Needlide <max.litvinyuck553@gmail.com>
2023-09-13 20:35:35 +0300

    Configured routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { NewsComponent } from 'src/app/news/news.component';
+import { NasaGalleryComponent } from './nasa-gallery/nasa-gallery.component';
+import { MarsPhotosComponent } from './mars-photos/mars-photos.component';
+import { APODComponent } from './apod/apod.component';
+ 
+const routes: Routes = [
+  { path: 'News', component: NewsComponent },
+  { path: 'NasaGallery', component: NasaGalleryComponent },
+  { path: 'MarsPhotos', component: MarsPhotosComponent },
+  { path: 'APOD', component:APODComponent }
+  { path: '', redirectTo: '/News', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
