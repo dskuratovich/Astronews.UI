167d714bc2e8c4ac66b10628391f732d3e502697
Needlide <max.litvinyuck553@gmail.com>
2024-05-12 21:39:40 +0300

    Created source manager service to implement news source banning feature

---
+++ src/app/source-manager.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SourceManagerService {
+  private readonly Keyword = 'BannedSources';
+
+  constructor() {}
+
+  banSource(sourceName: string): void {
+    let jsonString: string | null = localStorage.getItem(this.Keyword);
+
+    let bannedSourcesList: string[] = [];
+
+    if (jsonString !== null) {
+      bannedSourcesList = JSON.parse(jsonString);
+    }
+
+    bannedSourcesList.push(sourceName);
+
+    localStorage.setItem(this.Keyword, JSON.stringify(bannedSourcesList));
+  }
+
+  unbanSource(sourceName: string): boolean {
+    let jsonString: string | null = localStorage.getItem(this.Keyword);
+
+    if (jsonString !== null) {
+      let bannedSourcesList: string[] = JSON.parse(jsonString);
+  
+      const indexToRemove = bannedSourcesList.indexOf(sourceName);
+      
+      if (indexToRemove !== -1) {
+        bannedSourcesList.splice(indexToRemove, 1);
+        localStorage.setItem(this.Keyword, JSON.stringify(bannedSourcesList));
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
