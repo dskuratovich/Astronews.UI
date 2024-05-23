d45aa612cdf263b6a00920a75ecbcd15402a62a8
Needlide <max.litvinyuck553@gmail.com>
2024-05-23 19:47:13 +0300

    Optimized getNasaGallery and formatted code

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,10 +10,9 @@ import { GalleryRootModel } from './models/gallery.root.model';
 import { ApiKeyService } from './api-key.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
-
   private newsEndpoint = environment.api.newsEndpoint;
   private nasaEndpoint = environment.api.nasaEndpoint;
   private marsCuriosityEndpoint = environment.api.marsCuriosityEndpoint;
@@ -21,54 +20,79 @@ export class DataService {
   private marsSpiritEndpoint = environment.api.marsSpiritEndpoint;
   private apodEndpoint = environment.api.apodEndpoint;
 
-  constructor(private http: HttpClient, private apiKeyService: ApiKeyService) { }
+  constructor(private http: HttpClient, private apiKeyService: ApiKeyService) {}
 
   getNews(url: string): Observable<NewsRootModel> {
     if (!url) {
       let requestUrl = this.newsEndpoint.concat(`?limit=60&offset=0`);
-      return this.http.get<NewsRootModel>(requestUrl).pipe(catchError(this.handleError));
+      return this.http
+        .get<NewsRootModel>(requestUrl)
+        .pipe(catchError(this.handleError));
     }
     return this.http.get<NewsRootModel>(url).pipe(catchError(this.handleError));
   }
 
   getNasaGallery(url: string): Observable<GalleryRootModel> {
-    let date = new Date();
-    let utcYear = date.getUTCFullYear();
     if (!url) {
-      let requestUrl = this.nasaEndpoint.concat(`?year_start=${utcYear}&page_size=50`);
-      return this.http.get<GalleryRootModel>(requestUrl).pipe(catchError(this.handleError));
-
+      let date = new Date();
+      let utcYear = date.getUTCFullYear();
+      let requestUrl = this.nasaEndpoint.concat(
+        `?year_start=${utcYear}&page_size=50`
+      );
+      return this.http
+        .get<GalleryRootModel>(requestUrl)
+        .pipe(catchError(this.handleError));
     }
-    return this.http.get<GalleryRootModel>(url).pipe(catchError(this.handleError));
+    return this.http
+      .get<GalleryRootModel>(url)
+      .pipe(catchError(this.handleError));
   }
 
   getCuriosityMarsPhotos(sol: number): Observable<MarsRootModel> {
     let apiKey = this.apiKeyService.getApiKey();
-    let requestUrl = this.marsCuriosityEndpoint.concat(`?sol=${sol}&api_key=${apiKey}`);
-    return this.http.get<MarsRootModel>(requestUrl).pipe(catchError(this.handleError));
+    let requestUrl = this.marsCuriosityEndpoint.concat(
+      `?sol=${sol}&api_key=${apiKey}`
+    );
+    return this.http
+      .get<MarsRootModel>(requestUrl)
+      .pipe(catchError(this.handleError));
   }
 
   getOpportunityMarsPhotos(sol: number): Observable<MarsRootModel> {
     let apiKey = this.apiKeyService.getApiKey();
-    let requestUrl = this.marsOpportunityEndpoint.concat(`?sol=${sol}&api_key=${apiKey}`);
-    return this.http.get<MarsRootModel>(requestUrl).pipe(catchError(this.handleError));
+    let requestUrl = this.marsOpportunityEndpoint.concat(
+      `?sol=${sol}&api_key=${apiKey}`
+    );
+    return this.http
+      .get<MarsRootModel>(requestUrl)
+      .pipe(catchError(this.handleError));
   }
 
   getSpiritMarsPhotos(sol: number): Observable<MarsRootModel> {
     let apiKey = this.apiKeyService.getApiKey();
-    let requestUrl = this.marsSpiritEndpoint.concat(`?sol=${sol}&api_key=${apiKey}`);
-    return this.http.get<MarsRootModel>(requestUrl).pipe(catchError(this.handleError));
+    let requestUrl = this.marsSpiritEndpoint.concat(
+      `?sol=${sol}&api_key=${apiKey}`
+    );
+    return this.http
+      .get<MarsRootModel>(requestUrl)
+      .pipe(catchError(this.handleError));
   }
 
   getApods(startDate: string, endDate: string): Observable<ApodModel[]> {
     let apiKey = this.apiKeyService.getApiKey();
-    let requestUrl = this.apodEndpoint.concat(`?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`);
-    return this.http.get<ApodModel[]>(requestUrl).pipe(catchError(this.handleError));
+    let requestUrl = this.apodEndpoint.concat(
+      `?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`
+    );
+    return this.http
+      .get<ApodModel[]>(requestUrl)
+      .pipe(catchError(this.handleError));
   }
 
   private handleError(error: any): Observable<never> {
     console.error('An error occurred: ', error);
 
-    return throwError(() => new Error('Couldn\'t retrieve data; please try again later.'));
+    return throwError(
+      () => new Error("Couldn't retrieve data; please try again later.")
+    );
   }
 }
