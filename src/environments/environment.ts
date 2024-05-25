15cb70c59807e6cfeb40dfcc3431cbc2a02808fd
Needlide <max.litvinyuck553@gmail.com>
2024-05-25 21:57:38 +0300

    Added endpoints for latest photos from the Mars rovers

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,8 @@ export const environment = {
     marsOpportunityEndpoint: 'https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos',
     marsSpiritEndpoint: 'https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos',
     apodEndpoint: 'https://api.nasa.gov/planetary/apod',
+    marsCuriosityLatestEndpoint: 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos',
+    marsOpportunityLatestEndpoint: 'https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/latest_photos',
+    marsSpiritLatestEndpoint: 'https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/latest_photos'
   }
 };
