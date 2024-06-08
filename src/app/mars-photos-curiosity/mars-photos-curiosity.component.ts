daadf004a5c4dc3bb70b6ac26ca557810b4b5157
Needlide <max.litvinyuck553@gmail.com>
2024-06-08 14:33:51 +0300

    Optimized search algorithm, added variables to track which actions were triggered. Added 'await' for every async method to avoid discrepancy when using common variable. Changed cache keywords to URL, to avoid requesting data for same URL because keywords were different. Minor improvements in helper methods

--- src/app/mars-photos-curiosity/mars-photos-curiosity.component.ts
+++ src/app/mars-photos-curiosity/mars-photos-curiosity.component.ts
@@ -23,6 +23,8 @@ export class MarsPhotosCuriosityComponent {
   private last_sol: number = 0;
   private sol: number = 0;
   private solDirection: boolean = false;
+  private scrollDownTriggered: boolean = false;
+  private edSearch: boolean = false;
 
   constructor(
     private apiCaller: DataService,
@@ -32,22 +34,14 @@ export class MarsPhotosCuriosityComponent {
     private urlBuilder: UrlBuilderService,
     private searchService: SearchService
   ) {
-    console.log('Constructor started');
     searchService.searchTerm$.subscribe(this.callApiSearch.bind(this));
-    console.log('Constructor finished');
   }
 
   private parseSolDirection(value: string): boolean {
-    console.log('parseSolDirectionStarted');
-    if (value.trim().startsWith('+') || value.trim().endsWith('+')) {
-      return true;
-    } else {
-      return false;
-    }
+    return value.trim().startsWith('+') || value.trim().endsWith('+');
   }
 
   private parseCameraName(value: string): MarsRoverCameras | undefined {
-    console.log('parseCameraName started');
     switch (value.toUpperCase()) {
       case 'FHAZ':
         return CuriosityCameras.FHAZ;
@@ -63,45 +57,35 @@ export class MarsPhotosCuriosityComponent {
         return CuriosityCameras.MARDI;
       case 'NAVCAM':
         return CuriosityCameras.NAVCAM;
+      default:
+        return undefined;
     }
-    return undefined;
   }
 
   private handleSearch(term: string): string {
-    console.log('handle search started');
     const { property, value } = parseSearchTerm(term);
     if (property && value) {
-      console.log('property and value contain data');
       switch (property.toLowerCase()) {
         case 's':
-          console.log('prefix is s');
-
-          if (value.includes('+') || value.includes('-')) {
-            let valueSign = parseSearchValue(value);
-
-            if (valueSign[0].includes('+') || valueSign[0].includes('-')) {
-              this.sol = parseInt(valueSign[1]);
-              console.log('sol retrieved from second position');
-              console.log('sol in handleSearch set to: ' + this.sol);
-            } else {
-              this.sol = parseInt(valueSign[0]);
-              console.log('sol retrieved from first position');
-              console.log('sol in handleSearch set to: ' + this.sol);
-            }
-          } else {
-            console.log('value does not contain + or - signs');
-            this.sol = parseInt(value);
+          if (!this.scrollDownTriggered) {
+            const valueSign = parseSearchValue(value);
+            this.sol = parseInt(
+              valueSign[0].includes('+') || valueSign[0].includes('-')
+                ? valueSign[1]
+                : valueSign[0]
+            );
+            this.scrollDownTriggered = false;
           }
-
+          this.scrollDownTriggered = false;
           this.solDirection = this.parseSolDirection(value);
+          this.edSearch = false;
           return this.urlBuilder.getMarsUrl(
             this.sol.toString(),
             undefined,
             this.rover_type
           );
         case 'cn':
-          console.log('prefix is cn');
-          console.log('sol is: ' + this.sol);
+          this.edSearch = false;
           return this.urlBuilder.getMarsUrl(
             this.sol.toString(),
             undefined,
@@ -109,30 +93,25 @@ export class MarsPhotosCuriosityComponent {
             this.parseCameraName(value)
           );
         case 'ed':
-          console.log('prefix is ed');
+          this.edSearch = true;
           return this.urlBuilder.getMarsUrl(
             this.sol.toString(),
             value,
             this.rover_type
           );
+        default:
+          this.edSearch = false;
+          return '';
       }
-    } else if (!property && !value) {
-      console.log('empty search bar, returning empty string');
+    } else if (!value) {
+      this.edSearch = false;
       return '';
     } else {
-      console.log('prefix is empty, value is not');
       if (this.isISO8601Date(value)) {
-        console.log('value is date');
-        return this.urlBuilder.getMarsUrl(
-          this.sol.toString(),
-          value,
-          this.rover_type
-        );
-      } else if (
-        !this.isISO8601Date(value) &&
-        this.containsOnlyAlphabet(value)
-      ) {
-        console.log('value is camera');
+        this.edSearch = true;
+        return this.urlBuilder.getMarsUrl(undefined, value, this.rover_type);
+      } else if (this.containsOnlyAlphabet(value)) {
+        this.edSearch = false;
         return this.urlBuilder.getMarsUrl(
           this.sol.toString(),
           undefined,
@@ -140,124 +119,125 @@ export class MarsPhotosCuriosityComponent {
           this.parseCameraName(value)
         );
       } else {
-        console.log('value is sol');
+        this.edSearch = false;
         return this.urlBuilder.getMarsUrl(value, undefined, this.rover_type);
       }
     }
-    console.log('handle search finished');
-    return '';
   }
 
-  private async clearApiCall(
-    url: string,
-    key: string,
-    isLatest: boolean
-  ): Promise<void> {
-    console.log('clearApiCall started');
+  private async clearApiCall(url: string, isLatest: boolean): Promise<void> {
     try {
       if (isLatest) {
-        console.log('latest clearApiCall started');
-        const responseData$ = this.apiCaller.getMarsLatestPhotos(url);
-        const responseData = await lastValueFrom(responseData$);
-
+        const responseData = await lastValueFrom(
+          this.apiCaller.getMarsLatestPhotos(url)
+        );
         this.data = responseData.latest_photos;
-        if (this.data) {
-          if (this.data.length > 0) {
-            this.last_sol = this.data[0].sol;
-            this.sol = this.last_sol;
-            console.log('sol in clearApiCall is set to: ' + this.last_sol);
-          }
+        if (this.data.length > 0) {
+          this.last_sol = this.data[0].sol;
+          this.sol = this.last_sol;
         }
-        this.cacheService.set(key, this.data);
+        this.cacheService.set(url, this.data);
       } else {
-        console.log('normal clearApiCall started');
-        const responseData$ = this.apiCaller.getMarsPhotos(url);
-        const responseData = await lastValueFrom(responseData$);
-
+        const responseData = await lastValueFrom(
+          this.apiCaller.getMarsPhotos(url)
+        );
         this.data = responseData.photos;
-        console.log('data in normal clearApiCall assigned');
-        this.cacheService.set(key, this.data);
+        if (this.data.length > 0) {
+          this.sol = this.data[0].sol;
+        }
+        this.cacheService.set(url, this.data);
       }
     } catch (error) {
       this.errorService.sendError(
-        'Error occured during fetching the data. Please, try again shortly.'
+        'Error occurred during fetching the data. Please, try again shortly.'
       );
       this.router.navigate(['/Error']);
     }
-    console.log('clearApiCall finished');
   }
 
   private async apiCall(url: string): Promise<void> {
-    console.log('apiCall started');
     try {
-      const responseData$ = this.apiCaller.getMarsPhotos(url);
-      const responseData = await lastValueFrom(responseData$);
-
+      const responseData = await lastValueFrom(
+        this.apiCaller.getMarsPhotos(url)
+      );
       if (responseData.photos.length == 0) {
-        this.onScrollDown();
+        await this.onScrollDown();
+      } else {
+        this.data = [...this.data, ...responseData.photos];
+        if (this.data.length > 0) {
+          this.sol = this.data[0].sol;
+        }
+        this.cacheService.set(url, this.data);
       }
-
-      this.data = [...this.data, ...responseData.photos];
-      console.log('apiCall data joined to the existing data');
-
-      this.cacheService.set(this.cache_keyword, this.data);
     } catch (error) {
       this.errorService.sendError(
         'Error occurred during fetching the data. Please try again shortly.'
       );
       this.router.navigate(['/Error']);
     }
-    console.log('apiCall finished');
   }
 
-  private callApiSearch(term: string) {
-    console.log('callApiSearch started');
-    let url = this.handleSearch(term);
-    console.log(url);
+  private async callApiSearch(term: string) {
+    const url = this.handleSearch(term);
+
     if (url) {
-      this.clearApiCall(url, term, false);
+      const cache = this.cacheService.get(url);
+      if (cache) {
+        this.data = cache;
+        if (this.data.length > 0) {
+          this.sol = this.data[0].sol;
+        }
+      } else {
+        await this.clearApiCall(url, false);
+      }
     } else {
-      this.clearApiCall(
-        this.urlBuilder.getMarsLatestUrl(this.rover_type),
-        term,
-        true
-      );
+      let urlDef = this.urlBuilder.getMarsLatestUrl(this.rover_type);
+      let cacheDef = this.cacheService.get(urlDef);
+      if (cacheDef) {
+        this.data = cacheDef;
+        if (this.data.length > 0) {
+          this.sol = this.data[0].sol;
+        }
+      } else {
+        await this.clearApiCall(urlDef, true);
+      }
     }
-    console.log('callApiSearch finished');
   }
 
-  onScrollDown(): void {
-    console.log('onScrollDown started');
-    this.sol += this.solDirection ? 1 : -1;
-
-    if (this.sol < 0) {
-      console.log('sol is smaller than 0');
-      this.sol = 0;
-    }
-
-    let url = this.handleSearch(this.searchService.getSearchTerm());
-
-    console.log('URL from onScrollDown: ' + url);
-
-    if (url) {
-      this.apiCall(url);
+  async onScrollDown(): Promise<void> {
+    if (!this.edSearch) {
+      this.scrollDownTriggered = true;
+      this.sol += this.solDirection ? 1 : -1;
+      if (this.sol < 0) {
+        this.sol = 0;
+      }
+      const term = this.searchService.getSearchTerm();
+      const url = this.handleSearch(term);
+      if (url) {
+        await this.apiCall(url);
+      } else {
+        await this.apiCall(
+          this.urlBuilder.getMarsUrl(
+            this.sol.toString(),
+            undefined,
+            this.rover_type
+          )
+        );
+      }
     } else {
-      this.apiCall(
+      this.sol++;
+      await this.apiCall(
         this.urlBuilder.getMarsUrl(
           this.sol.toString(),
           undefined,
-          this.rover_type,
-          undefined
+          this.rover_type
         )
       );
     }
-    console.log('onScrollDown finished');
   }
 
   private isISO8601Date(dateString: string): boolean {
-    const iso8601Pattern = /^\d{4}-\d{2}-\d{2}$/;
-
-    return iso8601Pattern.test(dateString);
+    return /^\d{4}-\d{2}-\d{2}$/.test(dateString);
   }
 
   private containsOnlyAlphabet(input: string): boolean {
