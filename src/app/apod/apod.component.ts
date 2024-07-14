b8dc73ba8b362a53f19a061f57ea5bcf6423160e
Needlide <max.litvinyuck553@gmail.com>
2024-07-14 21:30:01 +0300

    Changed the code to match the new project standard (switched from checking property and value every time to search mode logic)

--- src/app/apod/apod.component.ts
+++ src/app/apod/apod.component.ts
@@ -18,6 +18,8 @@ export class APODComponent {
   data: ApodModel[] = [];
   date: Date;
   isDataUpdated: boolean = false;
+  private readonly CacheKeyword: string = 'APOD_DEFAULT';
+  isSearchMode: boolean = false;
 
   constructor(
     private apiCaller: DataService,
@@ -32,128 +34,139 @@ export class APODComponent {
   }
 
   private filterData(term: string): void {
-    const { property, value } = parseSearchTerm(term);
-    if (property === null && value != '') {
-      let cache = this.cacheService.get(value);
+    if (term && term.length > 2) {
+      const { property, value } = parseSearchTerm(term);
+      this.isSearchMode = value !== '';
 
-      if (cache && !this.isDataUpdated) {
-        this.data = cache;
-      } else {
-        this.data = this.cacheService
-          .get('apod')
-          .filter(
-            (item: ApodModel) =>
-              item.title.toLowerCase().includes(value.toLowerCase()) ||
-              item.explanation.toLowerCase().includes(value.toLowerCase())
-          );
+      if (this.isSearchMode && property != null) {
+        switch (property?.toLowerCase()) {
+          case 't':
+            let cache_t = this.cacheService.get(term);
 
-        this.cacheService.set(value, this.data);
-        this.isDataUpdated = false;
+            if (cache_t && !this.isDataUpdated) {
+              this.data = cache_t;
+            } else {
+              this.data = this.cacheService
+                .get(this.CacheKeyword)
+                .filter((item: ApodModel) =>
+                  item.title.toLowerCase().includes(value.toLowerCase())
+                );
+              this.cacheService.set(term, this.data);
+              this.isDataUpdated = false;
+            }
+            break;
+          case 'e':
+            let cache_e = this.cacheService.get(term);
+
+            if (cache_e && !this.isDataUpdated) {
+              this.data = cache_e;
+            } else {
+              this.data = this.cacheService
+                .get(this.CacheKeyword)
+                .filter((item: ApodModel) =>
+                  item.explanation.toLowerCase().includes(value.toLowerCase())
+                );
+              this.cacheService.set(term, this.data);
+              this.isDataUpdated = false;
+            }
+            break;
+          case 'c':
+            let cache_c = this.cacheService.get(term);
+
+            if (cache_c && !this.isDataUpdated) {
+              this.data = cache_c;
+            } else {
+              this.data = this.cacheService
+                .get(this.CacheKeyword)
+                .filter((item: ApodModel) =>
+                  item.copyright.toLowerCase().includes(value.toLowerCase())
+                );
+              this.cacheService.set(term, this.data);
+              this.isDataUpdated = false;
+            }
+            break;
+          case 'd':
+            let cache_d = this.cacheService.get(term);
+
+            if (cache_d && !this.isDataUpdated) {
+              this.data = cache_d;
+            } else {
+              this.data = this.cacheService
+                .get(this.CacheKeyword)
+                .filter((item: ApodModel) => item.date.includes(value));
+              this.cacheService.set(term, this.data);
+              this.isDataUpdated = false;
+            }
+            break;
+          default:
+            this.data = this.cacheService.get(this.CacheKeyword);
+            break;
+        }
+      } else if (this.isSearchMode) {
+        let cache = this.cacheService.get(value);
+
+        if (cache) {
+          this.data = cache;
+        } else {
+          this.data = this.cacheService
+            .get(this.CacheKeyword)
+            .filter(
+              (item: ApodModel) =>
+                item.title.includes(value) || item.explanation.includes(value)
+            );
+          this.cacheService.set(value, this.data);
+        }
       }
-    }
-    if (property === null && value == '') {
-      let cache = this.cacheService.get('apod');
+    } else {
+      let cache = this.cacheService.get(this.CacheKeyword);
 
       if (cache) {
         this.data = cache;
       } else {
-        let yearEnd = this.convertDateToString(this.date);
-        this.date.setMonth(this.date.getMonth() - 1);
-        let yearStart = this.convertDateToString(this.date);
-        this.apiCall(yearStart, yearEnd);
-      }
-    }
-    if (property != null && value != '') {
-      switch (property?.toLowerCase()) {
-        case 't':
-          let cache_t = this.cacheService.get(term);
-
-          if (cache_t && !this.isDataUpdated) {
-            this.data = cache_t;
-          } else {
-            this.data = this.cacheService
-              .get('apod')
-              .filter((item: ApodModel) =>
-                item.title.toLowerCase().includes(value.toLowerCase())
-              );
-            this.cacheService.set(term, this.data);
-            this.isDataUpdated = false;
-          }
-          break;
-        case 'e':
-          let cache_e = this.cacheService.get(term);
-
-          if (cache_e && !this.isDataUpdated) {
-            this.data = cache_e;
-          } else {
-            this.data = this.cacheService
-              .get('apod')
-              .filter((item: ApodModel) =>
-                item.explanation.toLowerCase().includes(value.toLowerCase())
-              );
-            this.cacheService.set(term, this.data);
-            this.isDataUpdated = false;
-          }
-          break;
-        case 'c':
-          let cache_c = this.cacheService.get(term);
-
-          if (cache_c && !this.isDataUpdated) {
-            this.data = cache_c;
-          } else {
-            this.data = this.cacheService
-              .get('apod')
-              .filter((item: ApodModel) =>
-                item.copyright.toLowerCase().includes(value.toLowerCase())
-              );
-            this.cacheService.set(term, this.data);
-            this.isDataUpdated = false;
-          }
-          break;
-        case 'd':
-          let cache_d = this.cacheService.get(term);
-
-          if (cache_d && !this.isDataUpdated) {
-            this.data = cache_d;
-          } else {
-            this.data = this.cacheService
-              .get('apod')
-              .filter((item: ApodModel) => item.date.includes(value));
-            this.cacheService.set(term, this.data);
-            this.isDataUpdated = false;
-          }
-          break;
-        default:
-          this.data = this.cacheService.get('apod');
-          break;
+        if (!this.isDataUpdated) {
+          this.defaultCallApi();
+        }
       }
     }
   }
 
-  onScrollDown(): void {
+  async onScrollDown(): Promise<void> {
     this.date.setDate(this.date.getDate() - 1);
     let yearEnd = this.convertDateToString(this.date);
     this.date.setMonth(this.date.getMonth() - 1);
     let yearStart = this.convertDateToString(this.date);
-    this.apiCall(yearStart, yearEnd);
+    await this.apiCall(yearStart, yearEnd);
+    this.filterData(this.searchService.getSearchTerm());
   }
 
-  async apiCall(yearStart: string, yearEnd: string): Promise<void> {
+  private async apiCall(yearStart: string, yearEnd: string): Promise<void> {
     try {
       const responseData$ = this.apiCaller.getApods(yearStart, yearEnd);
       const responseData = await lastValueFrom(responseData$);
 
-      let cache = this.cacheService.get('apod');
+      if (responseData.length == 0) {
+        return;
+      }
+
+      let cache = this.cacheService.get(this.CacheKeyword);
 
       if (cache) {
         cache = [...cache, ...responseData];
-        this.cacheService.set('apod', cache);
+        this.cacheService.set(this.CacheKeyword, cache);
       } else {
-        this.cacheService.set('apod', responseData);
+        this.cacheService.set(this.CacheKeyword, responseData);
       }
+
+      let searchTerm = this.searchService.getSearchTerm();
+
+      if (searchTerm) {
+        this.filterData(searchTerm);
+      } else {
+        this.data = responseData;
+      }
+
       this.isDataUpdated = true;
 
-      this.filterData(this.searchService.getSearchTerm());
     } catch (error) {
       this.errorService.sendError(
         'Error occurred during fetching the data. Please, try again shortly.'
@@ -162,7 +175,7 @@ export class APODComponent {
     }
   }
 
-  convertDateToString(givenDate: Date): string {
+  private convertDateToString(givenDate: Date): string {
     let year = givenDate.getFullYear();
     let month = String(givenDate.getMonth() + 1).padStart(2, '0');
     let day = String(givenDate.getDate()).padStart(2, '0');
@@ -177,5 +190,13 @@ export class APODComponent {
 
   getSafeUrl(url: string): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
+  private defaultCallApi(): void {
+    let date_default = new Date();
+    let yearEnd = this.convertDateToString(date_default);
+    date_default.setMonth(this.date.getMonth() - 1);
+    let yearStart = this.convertDateToString(date_default);
+    this.apiCall(yearStart, yearEnd);
   }
 }
