68dd95f6c4ff5d834ffc8dc507701f03e7b63546
Needlide <max.litvinyuck553@gmail.com>
2024-07-21 15:55:08 +0300

    Formatted the file

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,20 @@
-import { Component, OnInit } from "@angular/core";
-import { SearchService } from "../search.service";
-import { Subject, debounceTime } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
   public isLightTheme = false;
   public searchTerm: string = '';
   searchSubject = new Subject<string>();
 
-  constructor(private searchService: SearchService) { }
+  constructor(private searchService: SearchService) {}
   ngOnInit(): void {
-    this.searchSubject.pipe(
-      debounceTime(570)
-    ).subscribe(searchText => {
+    this.searchSubject.pipe(debounceTime(570)).subscribe((searchText) => {
       this.searchService.setSearchTerm(searchText);
     });
   }
