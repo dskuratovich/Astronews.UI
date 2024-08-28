ef41fb9a44db20609c6ef0d18e240388212271da
Needlide <max.litvinyuck553@gmail.com>
2024-08-28 18:19:16 +0000

    Configured app to use the API key from Netlify

--- config.js
+++
@@ -1,3 +0,0 @@
-window.__env = {
-  NASA_API_KEY: process.env.NASA_API_KEY
-};
--- src/app/api-key.service.ts
+++ src/app/api-key.service.ts
@@ -6,21 +6,15 @@ import { environment } from '../environments/environment';
 })
 export class ApiKeyService {
 
-  private config: { [key: string]: string };
-  //private apiKey: string = environment.secrets.api_key;
+  private apiKey: string = environment.secrets.api_key;
   constructor() {
-    this.config = window['__env'] || {};
   }
 
-  /*setApiKey(key: string): void {
+  setApiKey(key: string): void {
     this.apiKey = key;
-  }*/
+  }
 
   getApiKey(): string {
-    return this.config['NASA_API_KEY'] || '';
-  }
-
-  /*getApiKey(): string {
     return this.apiKey || '';
-  }*/
+  }
 }
